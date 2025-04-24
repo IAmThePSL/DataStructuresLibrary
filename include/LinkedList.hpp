ecb3c0146c83ba7abdd6517d0192a4032dcc5d18
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-04-25 01:08:04 +0200

    Update LinkedList.hpp

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
 #include <iostream>
 
 struct Node {
@@ -10,6 +12,7 @@ struct Node {
 class LinkedList {
 private:
     Node* head;
+    int length;
 
 public:
     LinkedList();
@@ -17,8 +20,8 @@ public:
 
     void push_front(int value);            //insert at beginning
     void push_back(int value);             //insert at end
-    void pop_front();                      //remove first node
-    void pop_back();                       //remove last node
+    int pop_and_return_front();            //remove first node and return
+    int pop_and_return_back();             //remove last node and return
     bool search(int value);                //check if value exists
     void print() const;                    //print values
     void clear();                          //delete all nodes
@@ -28,12 +31,13 @@ public:
     void remove_at(int index);             //remove node at index
     void insert_at(int index, int value);  //insert node at index
     void reverse();                        //reverse list in-palce
-    int pop_front();                       //remove first and return
-    int pop_back();                        //remove last and return
     int find(int value) const;              //find first value index
     void remove_value(int value);          //delete first occurrence
     void set_at(int index, int value);     //update value at index
     bool contains(int value) const;        //check value existence
     bool equals(const LinkedList& other) const;//compare lists
     LinkedList clone() const;              //create deep copy
+    std::vector<int> to_vector() const;
 };
+
+#endif
