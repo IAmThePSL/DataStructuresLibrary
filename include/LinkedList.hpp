ac7d44d012ee6c033045f1aab099928d804f438b
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2025-04-30 17:04:40 +0200

    Update LinkedList.hpp

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -31,13 +31,15 @@ public:
     void remove_at(int index);             //remove node at index
     void insert_at(int index, int value);  //insert node at index
     void reverse();                        //reverse list in-palce
-    int find(int value) const;              //find first value index
+    int find(int value) const;             //find first value index
     void remove_value(int value);          //delete first occurrence
     void set_at(int index, int value);     //update value at index
     bool contains(int value) const;        //check value existence
     bool equals(const LinkedList& other) const;//compare lists
     LinkedList clone() const;              //create deep copy
     std::vector<int> to_vector() const;
+    LinkedList(const LinkedList& other);   //copy constructor
+    LinkedList& operator=(const LinkedList& other);//assignment
 };
 
 #endif
